649090ac7e191169b99869c595c96badf19620e4
Bagus Perwira <71205584+zdev2@users.noreply.github.com>
2023-08-24 18:21:54 +0800

    Update akarkuadrat.php

--- test/akarkuadrat.php
+++ test/akarkuadrat.php
@@ -1,6 +1,64 @@
 <?php
 //ax^2+bx+c=0
 echo "<style>
+.bullets {
+    height: 10px;
+    position: relative;
+    border: none;
+  }
+  
+  .center {
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+  }
+.button {
+    border: none;
+    color: white;
+    padding: 4px 16px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    transition-duration: 0.4s;
+    cursor: pointer;
+
+    
+  }
+  
+  .button1 {
+    background-color: white;
+    color: black;
+    border: 2px solid #ffad33;
+   
+  }
+  
+  .button1:hover {
+    background-color: #ffad33;
+    color: white;
+
+  }
+  
+  .button2 {
+    background-color: white;
+    color: black;
+    border: 2px solid #ff3333;
+
+  }
+  
+  .button2:hover {
+    background-color: #ff3333;
+    color: white;
+
+  }
+p {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+body {background-color: #E0FFFF;}
 h1 {text-align: center;}
 h4 {text-align: left;}
 p {text-align: center;}
@@ -39,4 +97,7 @@ else {
     echo "Akar Tidak Real / Imajiner$br30";
 }
 echo "<footer><p>Nasi Goreng Cap Pak Hidayat</p></footer>";
+echo "<div class='bullets'><div class='center'>
+<a href='https://github.com/zdev2'><button class='button button1'>Github</button></a>
+<a href='https://www.youtube.com/@bagusbilek'><button class='button button2'>Youtube</button></a></div></div>";
 ?>
