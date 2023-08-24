29ae670c8aa479eef382f988ce26a41f66fb1a33
Bagus Perwira <71205584+zdev2@users.noreply.github.com>
2023-08-24 16:25:12 +0800

    Update akarkuadrat.php

--- test/akarkuadrat.php
+++ test/akarkuadrat.php
@@ -8,6 +8,7 @@ echo "<h1>Mencari Persamaan Akar Kuadrat<br>";
 $a = 4;
 $b = 15;
 $c = 9;
+$br30 = "<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>";
 echo "--------------------------------------<br><h4>Bagus Perwira/X RPL 2/05<br><br>";
 
 echo "A = $a<br>B = $b<br>C = $c<br><br>";
@@ -17,16 +18,16 @@ if ($D > 0){
     echo "Akar Real dan Berbeda<br>";
     $x1 = round(-$b + sqrt($D)/(2*$a));
     $x2 = round(-$b - sqrt($D)/(2*$a));
-    echo "x1 = $x1<br>x2 = $x2<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>";
+    echo "x1 = $x1<br>x2 = $x2$br30";
 }
 elseif ($D > 0){
     echo "Akar Real dan Sama";
     $x1 = round(-$b + sqrt($D)/(2*$a));
     $x2 = $x1;
-    echo "x1 = $x1<br>x2 = $x2<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>";
+    echo "x1 = $x1<br>x2 = $x2$br30";
 }
 else {
-    echo "Akar Tidak Real / Imajiner<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>";
+    echo "Akar Tidak Real / Imajiner$br30";
 }
 echo "<footer><p>Nasi Goreng Cap Pak Hidayat</p></footer>";
 ?>
